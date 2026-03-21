cbc342a844ca1771eeb98412c369f123e8199499
Vallepu Vamsi <vallepuvamsi2003@gmail.com>
2026-03-21 11:59:25 +0530

    Time: 0 ms (100%), Space: 20.6 MB (50.6%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        i = 0  # position to place next non-zero
+        
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+        
