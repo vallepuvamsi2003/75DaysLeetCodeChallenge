e3eb08ead2e85caf0981737187b5ad099aafb93a
Vallepu Vamsi <vallepuvamsi2003@gmail.com>
2026-03-31 18:20:17 +0530

    Time: 1 ms (87.59%), Space: 22.4 MB (89.34%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,23 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+        # Push to min stack if it's smaller or equal
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
