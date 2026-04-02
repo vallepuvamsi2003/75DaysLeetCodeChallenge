56d898352b355b3077189f09e8ceb0218d0452db
Vallepu Vamsi <vallepuvamsi2003@gmail.com>
2026-04-02 22:52:11 +0530

    Time: 83 ms (91.97%), Space: 28.2 MB (84.93%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        result = [0] * n
+        stack = []  # stores indices
+
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev_index = stack.pop()
+                result[prev_index] = i - prev_index
+            
+            stack.append(i)
+
+        return result
+        
