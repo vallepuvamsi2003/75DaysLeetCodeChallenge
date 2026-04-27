da5d9b94a70e5ea894ac38d3eb3a92909ba1541f
Vallepu Vamsi <vallepuvamsi2003@gmail.com>
2026-04-27 18:05:53 +0530

    Time: 39 ms (54.3%), Space: 34 MB (71.79%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -0,0 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            
+            node = node.children[char]
+        
+        node.isEnd = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        
+        return node.isEnd
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        
+        return True
