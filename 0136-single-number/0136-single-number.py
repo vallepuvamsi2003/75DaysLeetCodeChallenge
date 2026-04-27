eb5877b2d27afb493994c303941679c888a616ff
Vallepu Vamsi <vallepuvamsi2003@gmail.com>
2026-04-27 18:09:54 +0530

    Time: 1 ms (70.24%), Space: 21.2 MB (46.9%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+
+        for num in nums:
+            result ^= num   # XOR operation
+
+        return result
+        
